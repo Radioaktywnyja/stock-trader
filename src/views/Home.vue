192442e4e146c299c24d25f93c017316e4e3096c
Radioaktywnyja <milosz.kusak@gmail.com>
2019-09-09 21:51:34 +0200

    Added template for Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="app">
-    Stock Trader
+  <div>
+    <b-card bg-variant="light" border-variant="dark" class="mt-4">
+      <h2 class="font-weight-bold">Trade or View your Portfolio</h2>
+      <p class="my-0">You may Save & Load your Data</p>
+      <p class="my-0">Click on "End Day" to begin a new Day!</p>
+      <hr>
+      <p class="font-weight-bold">Your Funds: $10.000</p>
+    </b-card>
   </div>
 </template>
