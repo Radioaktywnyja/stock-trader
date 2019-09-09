1a48e35c7a06bde9bb8e9c33c8854248a7fa2812
Radioaktywnyja <milosz.kusak@gmail.com>
2019-09-09 23:10:29 +0200

    Added Stocks and StockItem template

---
+++ src/components/StockItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <b-card no-body class="rounded my-3 mx-auto p-0 col-sm-5">
+        <b-card-header class="bg-green py-1">
+            <span class="h4 text-success">{{ stockName }} </span><span>(Price: {{ stockPrice }})</span>
+        </b-card-header>
+        <b-card-body class="py-2">
+            <b-form inline>
+                <label class="sr-only" for="stock-count">Count</label>
+                <b-input id="stock-count" placeholder="Quantity" class="col-7"></b-input>
+                <b-button variant="success" class="ml-auto">Buy</b-button>
+            </b-form>
+        </b-card-body>
+    </b-card>
+</template>
+
+<script>
+    export default {
+        props: {
+            stockName: String,
+            stockPrice: Number
+        }
+    }
+</script>
+
+<style scoped>
+    .rounded {
+        border-color: #baecb3;
+    }
+
+    .bg-green {
+        background-color: #baecb3;
+        border-color: #baecb3;
+    }
+</style>
---
+++ src/views/Stocks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row px-3">
+    <stock-item stockName="BMW" stockPrice="110"></stock-item>
+    <stock-item stockName="Google" stockPrice="200"></stock-item>
+    <stock-item stockName="Apple" stockPrice="250"></stock-item>
+    <stock-item stockName="Twitter" stockPrice="8"></stock-item>
+  </div>
+</template>
+
+<script>
+  import StockItem from '../components/StockItem';
+
+  export default {
+    components: {
+      stockItem: StockItem
+    }
+  }
+</script>
