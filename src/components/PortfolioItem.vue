040169112aa06a1eb50da7d8cc3c0ecd5f3b6d04
Radioaktywnyja <milosz.kusak@gmail.com>
2019-09-09 23:36:19 +0200

    Added Portfolio and PortfolioItem template

---
+++ src/components/PortfolioItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <b-card no-body class="rounded my-3 mx-auto p-0 col-md-5">
+        <b-card-header class="bg-blue py-1">
+            <span class="h4 text-primary">{{ stockName }} </span><span>(Price: {{ stockPrice }} | Quantity: {{ stockQuantity }})</span>
+        </b-card-header>
+        <b-card-body class="py-2">
+            <b-form inline>
+                <label class="sr-only" for="stock-count">Count</label>
+                <b-input id="stock-count" placeholder="Quantity" class="col-7"></b-input>
+                <b-button variant="danger" class="ml-auto">Sell</b-button>
+            </b-form>
+        </b-card-body>
+    </b-card>
+</template>
+
+<script>
+    export default {
+        props: {
+            stockName: String,
+            stockPrice: Number,
+            stockQuantity: Number
+        }
+    }
+</script>
+
+<style scoped>
+    .rounded {
+        border-color: #9fdef1;
+    }
+
+    .bg-blue {
+        background-color: #9fdef1;
+        border-color: #9fdef1;
+        font-size: 0.7rem;
+    }
+</style>
---
+++ src/views/Portfolio.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row px-3">
+    <portfolio-item stockName="BMW" stockPrice="110" stockQuantity="10"></portfolio-item>
+    <portfolio-item stockName="Google" stockPrice="200"  stockQuantity="15"></portfolio-item>
+  </div>
+</template>
+
+<script>
+  import PortfolioItem from '../components/PortfolioItem';
+
+  export default {
+    components: {
+      portfolioItem: PortfolioItem
+    }
+  }
+</script>
