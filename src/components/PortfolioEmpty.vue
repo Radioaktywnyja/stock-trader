6bf27c0952ebeec06ea9f91c70c4d2cb88771009
Radioaktywnyja <milosz.kusak@gmail.com>
2019-09-11 10:13:30 +0200

    Added info about empty portfolio

---
+++ src/components/PortfolioEmpty.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="mt-3 mx-auto">
+    <b-card bg-variant="light" border-variant="dark" class="text-center">
+      <h2 class="font-weight-bold">Your portfolio is empty.</h2></h2>
+      <p class="my-0">Visit Stocks section and buy something.</p>
+    </b-card>
+  </div>
+</template>
